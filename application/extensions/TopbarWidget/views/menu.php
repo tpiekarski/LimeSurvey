44e017d15562e2500b263610a06992a919109d78
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2020-09-28 16:18:10 +0200

    Dev: Factor out dropdown partial view

--- application/extensions/TopbarWidget/views/menu.php
+++ application/extensions/TopbarWidget/views/menu.php
@@ -3,39 +3,7 @@
         <div class="topbarbuttongroup btn-group" href="#">
             <div class="topbardropdown">
                 <div class="topbarbutton">
-                    <button
-                        type="button"
-                        class="btn btn-default navbar-btn button dropdown-toggle"
-                        data-toggle="dropdown"
-                        aria-haspopup="true"
-                        aria-expanded="false"
-                        >
-                        <?php if ($button->getIconClass()) : ?>
-                            <i class="<?= $button->getIconClass(); ?>"></i>&nbsp;
-                        <?php endif; ?>
-                        <?= $button->getLabel(); ?>
-                        &nbsp;
-                        <i class="caret"></i>
-                    </button>
-                    <ul class="dropdown-menu" role="menu">
-                        <?php foreach ($button->getMenuItems() as $menuItem) : ?>
-                            <?php if ($menuItem->isDivider()) : ?>
-                                <li class="divider"></li>
-                            <?php elseif ($menuItem->isSmallText()) : ?>
-                                <li class="dropdown-header"><?= $menuItem->getLabel(); ?></li>
-                            <?php else : ?>
-                                <li>
-                                    <a href="<?= $menuItem->getHref(); ?>">
-                                        <!-- Spit out icon if present -->
-                                        <?php if ($menuItem->getIconClass() != '') : ?>
-                                          <i class="<?= $menuItem->getIconClass(); ?>">&nbsp;</i>
-                                        <?php endif; ?>
-                                        <?= $menuItem->getLabel(); ?>
-                                    </a>
-                                </li>
-                            <?php endif; ?>
-                        <?php endforeach; ?>
-                    </ul>
+                    <?php $this->render('dropdown', ['button' => $button]); ?>
                 </div>
             </div>
         </div>
